fc5ec0b172b95aecd8c64ac0372e5bae12e80147
Jannis R <mail@jannisr.de>
2017-09-20 18:16:58 +0200

    also accept 7 digits

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 'use strict'
 
-const to12Digit = (nine) => {
-	if ('string' !== typeof nine) throw new Error('ID must be a string.')
-	if (nine.length !== 9) throw new Error('ID must have 9 digits.')
+const to12Digit = (id) => {
+	if ('string' !== typeof id) throw new Error('ID must be a string.')
+	if (id.length === 12) return id
+	if (id.length === 9) return id.slice(0, 1) + '000' + id.slice(1)
+	if (id.length === 7) return id.slice(0, 1) + '00000' + id.slice(1)
 
-	return nine.slice(0, 1) + '000' + nine.slice(1)
+	throw new Error('ID must have 7 or 9 digits.')
 }
 
 const to9Digit = (twelve) => {
 	if ('string' !== typeof twelve) throw new Error('ID must be a string.')
-	if (twelve.length !== 12) throw new Error('ID must have 12 digits.')
+	if (twelve.length === 9) return twelve
+	if (twelve.length === 12) return twelve.slice(0, 1) + twelve.slice(4)
+	if (twelve.length === 7) return twelve.slice(0, 1) + '00' + twelve.slice(1)
 
-	return twelve.slice(0, 1) + twelve.slice(4)
+	throw new Error('ID must have 7 or 12 digits.')
 }
 
 module.exports = {to12Digit, to9Digit}
--- test.js
+++ test.js
@@ -7,3 +7,5 @@ const {to12Digit, to9Digit} = require('.')
 // Berlin Hauptbahnhof
 assert.strictEqual(to12Digit('900003201'), '900000003201')
 assert.strictEqual(to9Digit('900000003201'), '900003201')
+assert.strictEqual(to9Digit('9003201'), '900003201')
+assert.strictEqual(to12Digit('9003201'), '900000003201')
